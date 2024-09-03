2122b53a9e9a8c0b2c24a2f1c1a2be4c99117ad3
Hans Strausl <strauslh@gmail.com>
2024-09-02 22:28:11 -0700

    const termilock-passfile

--- src/pass.rs
+++ src/pass.rs
@@ -1,10 +1,9 @@
 use std::fs::{self};
 use std::io;
-
 use sha3::{Digest, Sha3_256};
-
-
 use std::io::prelude::*;
+
+const PASSWORD_FILE: &str = "/tmp/termilock-passfile.txt";
 
 pub fn hash_pass(pass: &str) -> String {
     //    let mut s = DefaultHasher::new();
@@ -23,13 +22,13 @@ pub fn hash_pass(pass: &str) -> String {
 pub fn set_password(pass: &str) -> io::Result<()> {
     // let mut file = get_password_file()?;
     // file.write(pass.as_bytes())?;
-    let mut file = fs::File::create("res/passfile.txt")?;
+    let mut file = fs::File::create(PASSWORD_FILE)?;
     let hashed = hash_pass(pass);
     file.write_all(hashed.as_bytes())
 }
 
 fn get_password_file() -> io::Result<fs::File> {
-    fs::File::open("res/passfile.txt")
+    fs::File::open(PASSWORD_FILE)
 }
 
 pub fn get_password() -> io::Result<String> {
